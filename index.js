d8dd152efec4cca361707bee78ed4f38bacce143
Parth Patel <42159154+patel1643@users.noreply.github.com>
2019-11-08 20:12:40 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+
+x = document.querySelectorAll("button")
+
+for(var i = 0; i < x.length; i++){
+  x[i].addEventListener("click", function (){
+    var buttonName = this.innerHTML;
+    makeSound(buttonName);
+  })
+}
+
+
+document.addEventListener("keypress", function(event){
+  makeSound(event.key);
+})
+
+function makeSound(key){
+  switch (key){
+
+    case "w":
+    var wSound = new Audio('sounds/crash.mp3');
+    wSound.play();
+    break;
+
+    case "a":
+     var aSound = new Audio('sounds/kick-bass.mp3');
+     aSound.play();
+     break;
+
+     case "s":
+     var sSound = new Audio('sounds/snare.mp3');
+     sSound.play();
+     break;
+
+     case "d":
+     var dSound = new Audio('sounds/tom-1.mp3');
+     dSound.play();
+     break;
+
+     case "j":
+      var jSound = new Audio('sounds/tom-2.mp3');
+      jSound.play();
+      break;
+
+      case "k":
+      var kSound = new Audio('sounds/tom-3.mp3');
+      kSound.play();
+      break;
+
+      case "l":
+      var lSound = new Audio('sounds/tom-4.mp3');
+      lSound.play();
+      break;
+
+  }
+}
